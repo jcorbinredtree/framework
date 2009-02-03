1be48f16fc8b37a90290e8a395f098f4421aa8ab
Joshua T Corbin <jcorbin@redtreesystems.com>
2009-02-03 00:49:06 +0000

    Create cache directory if it doesn't exist.
    In ApplicationData::getDataFile, if cacheDir doesn't appear to be writable, do
    an additional check to see if there's even a directory to be writing to, try
    to create it if it doesn't exist.

--- lib/application/ApplicationData.php
+++ lib/application/ApplicationData.php
@@ -50,7 +50,17 @@ class ApplicationData
         $policy = PolicyManager::getInstance();
         $dir = $policy->getCacheDir();
         if (!is_writable($dir)) {
-            throw new Exception("$dir is not writable");
+            if (is_dir($dir)) {
+                throw new Exception("$dir is not writable");
+            }
+
+            ob_start();
+            if (! mkdir($dir, 0777, true)) {
+                $mess = ob_get_clean();
+                throw new Exception("Could not create $dir: $mess");
+            } else {
+                ob_end_flush();
+            }
         }
         
         return "$dir/application.data";        
