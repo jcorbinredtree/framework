2b74de4578ede6196f99b06a3586c54d6c66eca4
Joshua T Corbin <jcorbin@redtreesystems.com>
2009-02-03 11:04:34 -0500

    Add missing license notices.

--- lib/ui/Template.php
+++ lib/ui/Template.php
@@ -5,10 +5,17 @@
  *
  * PHP version 5
  *
- * LICENSE: This file is a part of the Red Tree Systems framework,
- * and is licensed royalty free to customers who have purchased
- * services from us. Please see http://www.redtreesystems.com for
- * details.
+ * LICENSE: The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is Red Tree Systems Code.
+ *
+ * The Initial Developer of the Original Code is Red Tree Systems, LLC. All Rights Reserved.
  *
  * @category     UI
  * @package      Utils
