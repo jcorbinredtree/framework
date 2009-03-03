e8cb6fd18e6944f921fa09977f0bd4a0cf2758dd
Joshua T Corbin <jcorbin@redtreesystems.com>
2009-03-03 17:24:35 -0500

    Add new database object linking abstract

---
+++ lib/database/DatabaseObjectLink.php
@@ -0,0 +1,287 @@
+<?php
+
+/**
+ * Database Object class definition
+ *
+ * PHP version 5
+ *
+ * LICENSE: The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is Red Tree Systems Code.
+ *
+ * The Initial Developer of the Original Code is Red Tree Systems, LLC. All Rights Reserved.
+ *
+ * @author       Red Tree Systems, LLC <support@redtreesystems.com>
+ * @copyright    2007 Red Tree Systems, LLC
+ * @license      MPL 1.1
+ * @version      1.0
+ * @link         http://framework.redtreesystems.com
+ */
+
+require_once 'lib/database/DatabaseObjectLinkMeta.php';
+
+/**
+ * Represents managed links between database objects
+ *
+ * Example:
+ *   class DBOA extends DatabaseObject
+ *   {
+ *     public static $table = 'obj_a';
+ *     public static $key = 'obj_a_id';
+ *   }
+ *
+ *   class DBOB extends DatabaseObject
+ *   {
+ *     public static $table = 'obj_b';
+ *     public static $key = 'obj_b_id';
+ *   }
+ *
+ *   class DBOABLink extends DatabaseObjectLink
+ *   {
+ *     public static $FromClass = 'DBOA';
+ *     public static $ToClass = 'DBOB';
+ *   }
+ *
+ *   FIXME
+ */
+abstract class DatabaseObjectLink
+{
+    protected $from;
+    protected $to;
+
+    public static function deleteFor($linkClass, DatabaseObject $for)
+    {
+        if (
+            ! class_exists($linkClass) ||
+            ! is_subclass_of($linkClass, __CLASS__)
+        ) {
+            throw new InvalidArgumentException('invalid linkClass');
+        }
+        if (! isset($for->id)) {
+            throw new InvalidArgumentException('unsaved DatabaseObject');
+        }
+
+        global $database;
+        $meta = DatabaseObjectLinkMeta::forClass($linkClass);
+        $ok = false;
+        if (is_a($for, $meta->getFromClass())) {
+            $ok = true;
+            $sql = $meta->getSQL('link_delete_from');
+            $database->executef($sql, $for->id);
+        }
+        if (is_a($for, $meta->getToClass())) {
+            $ok = true;
+            $sql = $meta->getSQL('link_delete_to');
+            $database->executef($sql, $for->id);
+        }
+        if (! $ok) {
+            throw new InvalidArgumentException(
+                'invalid DatabaseObject, expecting a '.$meta->getFromClass().
+                'or a '.$meta->getToClass()
+            );
+        }
+    }
+
+    public static function loadFor($linkClass, DatabaseObject $for)
+    {
+        if (
+            ! class_exists($linkClass) ||
+            ! is_subclass_of($linkClass, __CLASS__)
+        ) {
+            throw new InvalidArgumentException('invalid linkClass');
+        }
+        if (! isset($for->id)) {
+            throw new InvalidArgumentException('unsaved DatabaseObject');
+        }
+
+        global $database;
+        $meta = DatabaseObjectLinkMeta::forClass($linkClass);
+        $data = array();
+        if (is_a($for, $meta->getFromClass())) {
+            $sql = $meta->getSQL('link_load_from');
+            $data['from'] = $for;
+            $other = 'to';
+            $otherClass = $meta->getToClass();
+        } elseif (is_a($for, $meta->getToClass())) {
+            $sql = $meta->getSQL('link_load_to');
+            $data['to'] = $for;
+            $other = 'from';
+            $otherClass = $meta->getFromClass();
+        } else {
+            throw new InvalidArgumentException(
+                'invalid DatabaseObject, expecting a '.$meta->getFromClass().
+                'or a '.$meta->getToClass()
+            );
+        }
+
+        $otherId = null;
+        $list = $database->executef($sql, $for->id);
+        $database->free();
+
+        $list->bindColumn(1, $otherId);
+        $i = 1;
+        foreach (array_merge(
+            $meta->getAdditionalKey(),
+            $meta->getAutomaticColumns()
+        ) as $column) {
+            $data[$column] = null;
+            $list->bindColumn(++$i, $data[$column]);
+        }
+
+        $r = array();
+        $call = array($linkClass, 'factory');
+        while ($list->fetch()) {
+            $data[$other] = DatabaseObject::load($otherClass, $otherId);
+            array_push($r, call_user_func($call, $data));
+        }
+        return $r;
+    }
+
+    abstract static protected function factory($data);
+
+    public static $LinkOrderClause = array('{from_key}', '{to_key}');
+
+    public function __construct($from, $to)
+    {
+        $meta = $this->meta();
+        if (is_array($from)) { // loading not creating
+            assert(array_key_exists('from', $from));
+            assert(array_key_exists('to', $from));
+            $this->from = $from['from'];
+            $this->to = $from['to'];
+            foreach ($meta->getColumnMap() as $prop => $col) {
+                if (array_key_exists($col, $from)) {
+                    $this->$prop = $from[$col];
+                }
+            }
+        } else {
+            if (! is_a($from, $meta->getFromClass())) {
+                throw new InvalidArgumentException(
+                    'invalid from object expecting a '.$meta->getFromClass()
+                );
+            }
+            if (! is_a($to, $meta->getToClass())) {
+                throw new InvalidArgumentException(
+                    'invalid to object expecting a '.$meta->getToClass()
+                );
+            }
+            if (! isset($from->id)) {
+                throw new InvalidArgumentException(
+                    'Can\'t link unsaved '.get_class($from)
+                );
+            }
+            if (! isset($to->id)) {
+                throw new InvalidArgumentException(
+                    'Can\'t link unsaved '.get_class($to)
+                );
+            }
+            $this->from = $from;
+            $this->to = $to;
+
+            $this->doInsert();
+        }
+    }
+
+    public function meta()
+    {
+        return DatabaseObjectLinkMeta::forClass(get_class($this));
+    }
+
+    protected function lockTables($database)
+    {
+        global $database;
+        $meta = $this->meta();
+        $database->lock(
+            array(
+                $meta->getFromMeta()->getTable(),
+                $meta->getToMeta()->getTable(),
+                $meta->getTable()
+            ), Database::LOCK_WRITE
+        );
+    }
+
+    protected function doInsert()
+    {
+        global $database;
+        $this->lockTables();
+        try {
+            $sql = $this->meta()->getSQL('link_insert');
+            $values = array_merge(
+                $this->keyValue(),
+                $this->autoValues()
+            );
+            $database->prepare($sql);
+            $database->execute($values);
+            $database->free();
+        } catch (Exception $e) {
+            $database->unlock();
+            throw $e;
+        }
+        $database->unlock();
+    }
+
+    public function update()
+    {
+        $meta = $this->meta();
+        if (! count($meta->getAutomaticColumns())) {
+            return;
+        }
+
+        global $database;
+        $sql = $this->meta()->getSQL('link_update');
+        $values = array_merge(
+            $this->keyValue(),
+            $this->autoValues()
+        );
+        $database->prepare($sql);
+        $database->execute($values);
+        $database->free();
+    }
+
+    public function delete()
+    {
+        global $database;
+        $sql = $this->meta()->getSQL('link_delete');
+        $database->prepare($sql);
+        $database->execute($this->keyValue());
+        $database->free();
+    }
+
+    protected function keyValue()
+    {
+        $r = array();
+        $r[$this->getFromKey()] = $this->from->id;
+        $r[$this->getToKey()] = $this->to->id;
+
+        $meta = $this->meta();
+        $addKey = $meta->getAdditionalKey();
+        if (isset($addKey)) {
+            $colmap = $meta->getColumnMap();
+            foreach ($addKey as $prop) {
+                $r[$colmap[$prop]] = $this->$prop;
+            }
+        }
+        return $r;
+    }
+
+    protected function autoValues()
+    {
+        $r = array();
+        $meta = $this->meta();
+        $colmap = $meta->getColumnMap();
+        foreach ($colmap as $prop => $col) {
+            if (! $this->isManualColumn($col)) {
+                $r[$col] = $this->$prop;
+            }
+        }
+        return $r;
+    }
+}
+
+?>
---
+++ lib/database/DatabaseObjectLinkMeta.php
@@ -0,0 +1,240 @@
+<?php
+
+/**
+ * DatabaseObjectLinkMeta class definition
+ *
+ * PHP version 5
+ *
+ * LICENSE: The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is Red Tree Systems Code.
+ *
+ * The Initial Developer of the Original Code is Red Tree Systems, LLC. All Rights Reserved.
+ *
+ * @author       Red Tree Systems, LLC <support@redtreesystems.com>
+ * @copyright    2007 Red Tree Systems, LLC
+ * @license      MPL 1.1
+ * @version      1.0
+ * @link         http://framework.redtreesystems.com
+ */
+
+require_once 'lib/database/DatabaseObjectAbstractMeta.php';
+
+class DatabaseObjectLinkMeta extends DatabaseObjectAbstractMeta
+{
+    /**
+     * Static storage for meta objects
+     */
+    static private $ClassMeta = array();
+
+    /**
+     * Static singlton manager
+     *
+     * Returns the meta object for a given DatabaseObjcetLink subclass
+     *
+     * @param class string the class
+     *
+     * @return DatabaseObjectLinkMeta
+     */
+    static public function forClass($class)
+    {
+        if (! array_key_exists($class, self::$ClassMeta)) {
+            if (! is_subclass_of($class, 'DatabaseObjectLink')) {
+                throw new InvalidArgumentException(
+                    "$class is not a DatabaseObjectLink subclass"
+                );
+            }
+
+            self::$ClassMeta[$class] =& new self($class);
+        }
+
+        return self::$ClassMeta[$class];
+    }
+
+    protected $queries = array(
+        'link_insert' =>
+            'INSERT INTO {table} SET {keyspec}{+fieldset}',
+        'link_update' =>
+            'UPDATE {table} SET {fieldset} WHERE {keyspec}',
+        'link_delete' =>
+            'DELETE FROM {table} WHERE {keyspec}',
+        'link_delete_from' =>
+            'DELETE FROM {table} WHERE {from_key}=?',
+        'link_delete_to' =>
+            'DELETE to {table} WHERE {to_key}=?',
+        'link_load_from' =>
+            'SELECT {to_key}{+addkey}{+colspec} FROM {table} WHERE {from_key}=? ORDER BY {orderfrom}',
+        'link_load_to' =>
+            'SELECT {from_key}{+addkey}{+colspec} FROM {table} WHERE {to_key}=? ORDER BY {orderto}'
+    );
+
+    protected $fromClass;
+    protected $toClass;
+    protected $additionalKey;
+    private $keyColumns;
+
+    public function __construct($class)
+    {
+        $refcls = new ReflectionClass($class);
+
+        $members = array();
+        foreach ($refcls->getProperties() as $prop) {
+            $name = $prop->getName();
+            switch ($name) {
+            case 'table':
+            case 'FromClass':
+            case 'ToClass':
+            case 'LinkOrderClause':
+            case 'AdditionalKey':
+                if (! $prop->isStatic() && $config->isDebugMode()) {
+                    throw new RuntimeException(
+                        "$class->$name should be $class::\$$name"
+                    );
+                }
+                $m = strtolower($name[0]).substr($name, 1);
+                $v = $prop->getValue();
+                if ($name == 'FromClass' || $name == 'ToClass') {
+                    if (
+                        ! class_exists($v) ||
+                        ! is_subclass_of($v, 'DatabaseObject')
+                    ) {
+                        throw new RuntimeException(
+                            "$v is not a DatabaseObject class"
+                        );
+                    }
+                }
+                $this->$m = $v;
+                break;
+            default:
+                if (! $prop->isStatic()) {
+                    array_push($members, $name);
+                }
+                break;
+            }
+        }
+
+        if (! isset($this->fromClass)) {
+            throw new RuntimeException(
+                "Cannot determine database fromClass for $class"
+            );
+        }
+        if (! isset($this->toClass)) {
+            throw new RuntimeException(
+                "Cannot determine database toClass for $class"
+            );
+        }
+
+        parent::__construct($class, $members);
+        $this->inspectColumn($this->getFromKey());
+        $this->inspectColumn($this->getToKey());
+
+        if (isset($this->additionalKey)) {
+            $man = isset($this->manualColumns)
+                ? $this->manualColumns : array();
+            $this->manualColumns = array_unique(array_merge(
+                $man, $this->additionalKey
+            ));
+            foreach ($this->additionalKey as $col) {
+                $this->inspectColumn($col);
+            }
+        }
+    }
+
+    public function getKeyColumns()
+    {
+        if (! isset($this->keyColumns)) {
+            $l = array(
+                $this->getFromKey(),
+                $this->getToKey()
+            );
+            if (isset($this->additionalKey)) {
+                $colmap = $this->getColumnMap();
+                foreach ($this->additionalKey as $addKey) {
+                    if (! array_key_exists($addKey, $colmap)) {
+                        throw new RuntimeException("$addKey column doesn't exist");
+                    }
+                    array_push($l, $colmap[$addKey]);
+                }
+            }
+            $this->keyColumns = $l;
+        }
+        return $this->keyColumns;
+    }
+
+    protected function expandSQL($sql)
+    {
+        $sql = parent::expandSQL($sql);
+
+        $auto = $this->getAutomaticColumns();
+        if (count($auto)) {
+            $colspec = ', '.$this->getColumnsSQL($auto, null);
+            $fieldset = ', '.$this->getFieldSetSQL($auto, false);
+        } else {
+            $colspec = '';
+            $fieldset = '';
+        }
+        $sql = str_replace('{+colspec}', $colspec, $sql);
+        $sql = str_replace('{+fieldset}', $fieldset, $sql);
+
+        if (isset($this->additionalKey)) {
+            $addkey = ', '.$this->getColumnsSQL($this->additionalKey, null);
+        } else {
+            $addkey = '';
+        }
+        $sql = str_replace('{+addkey}', $addkey, $sql);
+
+        $sql = str_replace('{orderfrom}', $this->linkOrderClause[0], $sql);
+        $sql = str_replace('{orderto}', $this->linkOrderClause[1], $sql);
+        $sql = str_replace('{from_key}', $this->getFromKey(), $sql);
+        $sql = str_replace('{to_key}', $this->getToKey(), $sql);
+
+        $keyList = $this->getKeyColumns();
+        $sql = str_replace('{keyspec}', $this->getFieldSetSQL($keyList), $sql);
+
+        return $sql;
+    }
+
+    public function getFromClass()
+    {
+        return $this->fromClass;
+    }
+
+    public function getToClass()
+    {
+        return $this->toClass;
+    }
+
+    public function getFromMeta()
+    {
+        return DatabaseObjectMeta::forClass($this->fromClass);
+    }
+
+    public function getToMeta()
+    {
+        return DatabaseObjectMeta::forClass($this->toClass);
+    }
+
+    public function getFromKey()
+    {
+        return $this->getFromMeta()->getTable().'_id';
+    }
+
+    public function getToKey()
+    {
+        return $this->getToMeta()->getTable().'_id';
+    }
+
+    public function getAdditionalKey()
+    {
+        return isset($this->additionalKey)
+            ? $this->additionalKey : array();
+    }
+}
+
+?>
